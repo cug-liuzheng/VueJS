277bcd34c878a39b023f8d8479e4cc1039f913cb
LZ <1617851696@qq.com>
2023-04-19 09:32:25 +0800

    界面基本搭建

---
+++ Vue3基础/02使用Vite/vue-project/src/methods.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>{{ msg }}</h1>
+    <button @click="changeMsg">点我一下</button>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg:"这是一个自定义的网址！"
+        }
+    },
+
+    /*
+        methods 用来指定实例对象中的方法
+            - 它是一个对象，可以在它里面定义多个方法
+            - 这些方法最终会被挂载到组件实例上
+            - 可以直接通过组件实例来调用这些方法
+            - methods 中函数的this会被自动绑定为组件实例
+    */
+    methods:{
+
+        test(){
+            console.log(this)   //组件实例， vm
+            alert('实例对象的方法')
+        },
+        changeMsg(){
+            this.msg = "新的消息！"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue3基础/Vue构建简单网页/src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+import {ref} from 'vue'
+
+// 创建一个变量来记录选项卡的状态
+const current = ref(0)    //0表示球员，1表示球队
+
+
+</script>
+<template>
+  <!-- 选项卡的外部容器 -->
+  <div class="tab-wrapper">
+    <!-- 选项卡的头部 -->
+    <header class="tab-head">
+      <!-- 定义两个按钮 -->
+      <div @click="current  = 0" class="tab-button" :class="{active:current === 0}">热门球员</div>
+      <div @click="current = 1" class="tab-button" :class="{active:current === 1}">热门球队</div>
+    </header>
+    <!-- 选项卡主体内容 -->
+    <div class="main">
+      <!-- 
+        v-show 指令，接受一个布尔值作为判断，用来设置一个内容是否显示
+          v-show是通过display来设置一个元素是否显示的
+       -->
+       <div v-show="current === 0">
+        <!-- 球员 -->
+        <div class="tab-lits">
+          <div class="tab-item">
+            <!-- 图片 -->
+            <div class="photo">
+              <img src="../public/images/messi.png" alt="梅西">
+              <span>1</span>
+            </div>
+            <!-- 描述 -->
+            <div class="desc">
+              <span class="name">梅西</span>
+              <div class="hot-bar">
+                <div class="inner">433760热度</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+      </div>
+       <div v-show="current === 1">
+        <!-- 球队 -->
+        <div class="tab-lits">
+          <div class="tab-item">
+            <!-- 图片 -->
+            <div class="photo">
+              <img src="../public/images/法国.jpg" alt="法国">
+              <span>1</span>
+            </div>
+            <!-- 描述 -->
+            <div class="desc">
+              <span class="name">法国</span>
+              <div class="hot-bar">
+                <div class="inner">433760热度</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .tab-wrapper {
+    box-sizing: border-box;
+    width: 800px;
+    padding: 20px;
+    background-color: rgb(45, 83, 211);
+  }
+
+  .tab-head {
+    display: flex;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+
+  .tab-button {
+    background-color: white;
+    font-size: 30px;
+    padding: 10px 0;
+    text-align: center;
+    flex: auto;
+    cursor: pointer;
+    transition: 0.5s;
+  }
+.tab-button:not(.active):hover {
+  color:rgb(187, 3, 52)
+}
+  .active {
+    background-color: rgb(187,3,52);
+    color:white
+  }
+</style>
